62d992e8ea88fbc70618b9ba6d2a90be209956d0
ele828 <ele828@gmail.com>
2017-08-29 10:34:39 +0800

    update

--- src/api/signOn.js
+++ src/api/signOn.js
@@ -3,18 +3,16 @@ import axios from 'axios'
 import { readFile, writeFile } from '../util/fs'
 
 // POST
-const SIGN_ON_API = 'user/signon?wc=true&remember=true'
+const SIGN_ON_API = 'https://ticktick.com/api/v2/user/signon?wc=true&remember=true'
 const TOKEN_CACHE = 'token.json'
 
 export default function signOn (axios) {
   return async (username, password) => {
     const token = JSON.parse(await readFile(path.join(TOKEN_CACHE)))
-    if (!token || !token.length) {
-      return token;
-    }
+    if (!token || !token.length) return token
     const res = await axios.post(SIGN_ON_API, { username, password })
     const _token = res.data.token
     await writeFile(path.join(TOKEN_CACHE), JSON.stringify(_token))
-    return _token;
+    return _token
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,26 +3,26 @@ import config from './config'
 import signOn from './api/signOn'
 import addTask from './api/task/addTask'
 
-class Ticktick {
-  constructor({
-    config
-  }) {
+// Delegates api endpoint to ticktick
+const apiList = { addTask }
+
+export default class Ticktick {
+  constructor(config) {
     this.config = config
     this.axios = axios.create({
       baseURL: 'https://ticktick.com/api/v2/',
       timeout: config.timeout,
     })
     this._signOn = signOn(this.axios)
-    this._addTask = addTask(this.axios)
+    // Inject api endpoint to ticktick application
+    for (const api of Object.keys(apiList)) {
+      this[api] = apiList[api].apply(this, [this.axios])
+    }
   }
 
   async signOn() {
     const token = await this._signOn(this.config.username, this.config.password)
     this.axios.defaults.headers.common['Cookie'] = `t=${token}`;
-  }
-
-  addTask(task) {
-    return this._addTask(task)
   }
 }
 
@@ -34,9 +34,7 @@ async function start () {
     timeout: 5000
   }
   try {
-    const ticktick = new Ticktick({
-      config: _config
-    })
+    const ticktick = new Ticktick(_config)
     await ticktick.signOn()
     await ticktick.addTask({
       title: 'test title',
@@ -48,4 +46,4 @@ async function start () {
   }
 }
 
-start();
+start()
